bbf195d1d5a37f708ed9544240d7014c6930f024
Watal M. Iwasaki <heavy.watal@gmail.com>
2018-02-01 14:58:30 +0900

    :sparkles: Add as_igraph()

---
+++ r-package/R/graph.R
@@ -0,0 +1,12 @@
+#' convert result to igraph
+#' @param .tbl result tibble
+#' @return igraph
+#' @rdname graph
+#' @export
+as_igraph = function(.tbl) {
+  .tbl %>%
+    dplyr::filter(.data$id > 0L) %>%
+    gather_chromosome() %>%
+    dplyr::select(.data$parent_id, .data$id) %>%
+    igraph::graph_from_data_frame()
+}
