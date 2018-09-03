460000d4f06e9f03dfabe7998090570a951e9f0e
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2018-09-03 18:01:49 +0900

    :white_check_mark: Check sizeof(Individual)

--- test/individual.cpp
+++ test/individual.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 
 int main() {
+    std::cout << "sizeof(Individual): " << sizeof(pbt::Individual) << "\n";
     pbt::Individual::set_default_values();
     pbt::Individual x;
     std::cout << x << std::endl;
