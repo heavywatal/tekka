7533bcd4d59b26b2144942643389e0b61b5796f3
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2018-12-28 12:01:10 +0900

    :art: Configure .cpp.in to avoid superfluous compilation

---
+++ src/config.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef PBT_CONFIG_HPP_
+#define PBT_CONFIG_HPP_
+
+namespace pbt {
+
+extern const char* const PROJECT_NAME;
+extern const char* const PROJECT_VERSION;
+
+extern const char* const default_values;
+
+}
+
+#endif // PBT_CONFIG_HPP_
