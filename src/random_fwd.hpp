19f354bd16882f96f7448f47c08bd6784bbe0326
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2024-11-09 23:26:07 +0900

    :zap: Use pcglite::pcg64 for random number generation

--- src/random_fwd.hpp
+++ src/random_fwd.hpp
@@ -3,9 +3,10 @@
 #define PBF_RANDOM_FWD_HPP
 
 #include <random>
+#include <pcglite/pcglite.hpp>
 
 namespace pbf {
-  using URBG = std::mt19937_64;
+  using URBG = pcglite::pcg64;
 }
 
 #endif//PBF_RANDOM_FWD_HPP
